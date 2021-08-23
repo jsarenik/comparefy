1c943861cbb4fd31bfde289fc69a00cb428d505c
Jan Sarenik <jajomojo@gmail.com>
2021-08-23 10:08:31 +0200

    Ethereum started in 2015

--- gen-datapoints-monthly-low.sh
+++ gen-datapoints-monthly-low.sh
@@ -4,10 +4,10 @@ a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; BINDIR=$(cd "$a" || exit; pwd)
 cd "$BINDIR" || exit
 
 YEARNOW=$(date +%Y)
-for year in $(seq 2009 $YEARNOW)
+for year in $(seq 2015 $YEARNOW)
 do
 for month in $(seq -w 01 12)
 do
-  grep -- "$year-$month-..\"" datapoints | sort -rn -t',' -k2 | tail -1
+  grep -- "$year-$month-..," datapoints-eth | sort -rn -t',' -k2 | tail -1
 done
 done
--- gen-datapoints-yearly-low.sh
+++ gen-datapoints-yearly-low.sh
@@ -4,7 +4,7 @@ a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; BINDIR=$(cd "$a" || exit; pwd)
 cd "$BINDIR" || exit
 
 YEARNOW=$(date +%Y)
-for year in $(seq 2009 $YEARNOW)
+for year in $(seq 2015 $YEARNOW)
 do
-  grep -- "$year-..-..\"" datapoints | sort -rn -t',' -k2 | tail -1
+  grep -- "$year-..-..," datapoints-eth | sort -rn -t',' -k2 | tail -1
 done
